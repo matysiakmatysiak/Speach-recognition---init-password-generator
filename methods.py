b9df171c095c9dd00a60e86b18cec7d1502e65a2
matysiakmatysiak <matysiak.michal96@gmail.com>
2022-05-15 19:02:35 +0200

    Added new files: gui.py, main.py. Recognition.py has been renamed to methods.py'

--- methods.py
+++ methods.py
@@ -3,9 +3,10 @@ import speech_recognition as sr
 import tkinter
 import sys
 
-#Funkcje
+
 def wyjscie():
     sys.exit()
+
 def record():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
@@ -17,16 +18,3 @@ def record():
     f.write(recognizer.recognize_google(audio, language="pl-PL"))
     f.write("\n")
     f.close()
-
-
-#GUI
-
-root = tkinter.Tk()
-root.geometry('1024x768')
-label = tkinter.Label(root, text='Speech Recognition by Michal')
-label.pack()
-b = tkinter.Button(root, text='Rozpocznij nagrywanie i zapisz do pliku', command=record)
-b.pack()
-b_2 = tkinter.Button(root, text='Wyjscie', command=wyjscie)
-b_2.pack()
-root.mainloop()
